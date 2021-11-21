73a581f1838afcdfd47811431fba4874d5d2cc5e
angappanmuthu <angappanmuthu232@gmail.com>
2021-11-21 22:53:57 +0530

    test1

---
+++ pico-test/main.cpp
@@ -0,0 +1,17 @@
+/*** Include ***/
+#include <cstdint>
+#include <cstdio>
+// #include <cstdlib>
+#include <cstring>
+
+#include "pico/stdlib.h"
+#include "tensorflow/lite/micro/all_ops_resolver.h"
+#include "tensorflow/lite/micro/micro_error_reporter.h"
+#include "tensorflow/lite/micro/micro_interpreter.h"
+#include "tensorflow/lite/schema/schema_generated.h"
+#include "tensorflow/lite/version.h"
+
+int main()
+{
+    return 0;
+}
